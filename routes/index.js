b72bad0b4aa8e8aeedb5816f7f59c0473ddf48b5
Fernando Durban <ferbrizie@gmail.com>
2023-06-06 16:51:08 +0200

    cambiosgerman

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ router.use('/cards', require('./card.routes'))
 router.use('/users', require('./user.routes'))
 router.use('/auth', require('./auth.routes'))
 router.use('/users', require('./user.routes'))
+// TODO ELIMINAR
 
 module.exports = router
